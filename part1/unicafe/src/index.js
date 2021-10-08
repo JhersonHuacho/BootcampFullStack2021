4194e827c152b95fa77c6f5892623d14ef9c129d
Francisco <jhersonhuacho@gmail.com>
2021-10-08 15:38:02 -0500

    1.7: unicafe paso 2

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const App = () => {
+  // save clicks of each button to it own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral]= useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleClickGood = () => {
+    setGood(good + 1);
+  }
+  const handleClickNeutral = () => {
+    setNeutral(neutral + 1);
+  }
+  const handleClickBad = () => {
+    setBad(bad + 1);
+  }
+
+  const all = good + neutral + bad;
+  const average = all === 0 ? 0 :(good - bad) / all;
+  const positive = all === 0 ? 0 : (good / all) * 100;
+  // console.log(good);
+  // console.log(neutral);
+  // console.log(bad);
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={handleClickGood}>good</button>
+      <button onClick={handleClickNeutral}>neutral</button>
+      <button onClick={handleClickBad}>bad</button>
+
+      <h1>statics</h1>
+
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all { all }</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
+    </div>
+  )
+
+
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
