367db6dc373d3136339c117c01d8f96171a9d930
Francisco <jhersonhuacho@gmail.com>
2021-11-12 11:35:46 -0500

    Part 2: Exercise 2.19 - 2.20

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,19 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 // Services
 import personsServices from './services/persons';
 // Components
 import { Filter } from './components/Filter';
 import { PersonForm } from './components/PersonForm';
 import { Persons } from './components/Persons';
+import Notification from './components/Notification';
 
 function App() {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [search, setSearch] = useState('');
+  const [message, setMessage] = useState(null);
+  const [isError, setIsError] = useState(false);
 
   const handleChangeName = (event) => setNewName(event.target.value);
   const handleChangeNumber = (event) => setNewNumber(event.target.value);
@@ -30,14 +33,34 @@ function App() {
       if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one`)) {
         personsServices.update(personExist.id, objNewPerson)
           .then(returnedPerson => {
+            setMessage(`Added ${objNewPerson.name}`);
+            setIsError(false);
+            setTimeout(() => {
+              setMessage(null);
+            }, 2000);
             setPersons(persons.map(person => person.id === returnedPerson.id ? returnedPerson : person));
             setNewName('');
             setNewNumber('');
           })
+          .catch(error => {
+            setMessage(`Information of ${objNewPerson.name} has already been removed from server`);
+            setIsError(true);
+            setTimeout(() => {
+              setMessage(null);
+            }, 2000);
+            setPersons(persons.filter(person => person.id !== personExist.id));
+            setNewName('');
+            setNewNumber('');
+          }) 
       }
     } else {
       personsServices.create(objNewPerson)
         .then(returnedPerson => {
+          setIsError(false);
+          setMessage(`Added ${objNewPerson.name}`);
+          setTimeout(() => {
+            setMessage(null);
+          }, 2000);
           setPersons(persons.concat(returnedPerson));
           setNewName('');
           setNewNumber('');
@@ -58,16 +81,17 @@ function App() {
 
   useEffect(() => {
     personsServices.getAll()
-      .then( initialPersons => setPersons(initialPersons));
+      .then(initialPersons => setPersons(initialPersons));
   }, [])
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={message} isError={isError} />
       <Filter search={search} handleChangeSearch={handleChangeSearch} />
       <h3>Add a New</h3>
       <PersonForm
-        newName={newName} 
+        newName={newName}
         newNumber={newNumber}
         handleChangeName={handleChangeName}
         handleChangeNumber={handleChangeNumber}
@@ -75,10 +99,10 @@ function App() {
       />
       <h3>Numbers</h3>
       <div>
-        <Persons 
-          persons={persons} 
+        <Persons
+          persons={persons}
           search={search}
-          handleClickDelete={handleClickDelete} 
+          handleClickDelete={handleClickDelete}
         />
       </div>
     </div>
